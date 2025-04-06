1728601e1b49aaec651e27f169ed3a431c5af700
jfegasu <jfegasu@gmail.com>
2025-04-06 11:15:31 -0500

    .

--- 4-DESARROLLO/app.py
+++ 4-DESARROLLO/app.py
@@ -6,7 +6,20 @@ from flask_cors import CORS
 app = Flask(__name__) 
 @app.route('/') 
 def raiz():   
-    return "HOLA"
+    return render_template('index.html')
+@app.route('/banner') 
+def banner():   
+    return render_template('banner.html')
+@app.route('/footer') 
+def footer():   
+    return render_template('footer.html')
+@app.route('/centro') 
+def centro():   
+    return render_template('centro.html')
+
+@app.route('/menu') 
+def menu():   
+    return render_template('menu.html')
 
 if __name__=='__main__':
     app.run(debug=True,port=5000,host='0.0.0.0')

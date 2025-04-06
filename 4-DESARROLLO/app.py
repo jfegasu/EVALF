28dcae2821e3d4e3319ac6a7e9a351ae5e9144f9
jfegasu <jfegasu@gmail.com>
2025-04-06 10:25:58 -0500

    Update app.py

---
+++ 4-DESARROLLO/app.py
@@ -0,0 +1,12 @@
+from flask import Flask,Blueprint, render_template,session,request ,redirect,url_for
+from flask_session import Session
+import pandas as pd
+from flask_cors import CORS
+from admin.routes import admin 
+app = Flask(__name__) 
+@app.route('/') 
+def raiz():   
+    return "HOLA"
+
+if __name__=='__main__':
+    app.run(debug=True,port=5000,host='0.0.0.0')

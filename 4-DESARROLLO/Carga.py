93391a347aaa3131a35afdf3bbace82f4f5b4629
jfegasu <jfegasu@gmail.com>
2025-04-12 16:56:05 -0500

    .

---
+++ 4-DESARROLLO/Carga.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from utils.Utilitarios import *
+print('CARGANDO APRENDICES')
+sql="DELETE FROM FICHAPRENDIZ"
+Insertar(sql)
+datos=pd.read_excel("CARGA.xlsx",sheet_name=1)
+# print(datos)
+aprendiz=pd.DataFrame(datos[['FICHA','DNI','NOMBRE','EMAIL','TITULACION']])
+aprendiz.drop_duplicates(inplace=True)
+aprendiz.to_csv("aprendiz.csv",index=False)
+for index, row in aprendiz.iterrows():
+    sql=f"INSERT INTO FICHAPRENDIZ(FICHA,DNIA,NOMBREAP,ESTADOAP,PWDAP,EMAIL,TITULACION) VALUES('{row['FICHA']}','{row['DNI']}','{row['NOMBRE']}',1,'{row['DNI']}','{row['EMAIL']}','{row['TITULACION']}')".format(row['FICHA'],row['DNI'],row['NOMBRE'],1,row['DNI'],row['EMAIL'],row['TITULACION'])
+    Insertar(sql)
+print('CARGANDO INSTRUCTORES')
+sql="DELETE FROM FICHAINSTRUCTOR"
+Insertar(sql)
+datos=pd.read_excel("CARGA.xlsx",sheet_name=0)
+instructor=pd.DataFrame(datos[['FICHA','DNI','NOMBRE','EMAIL']])
+instructor.drop_duplicates(inplace=True)
+instructor.to_csv("instructor.csv",index=False)
+for index, row in instructor.iterrows():
+    # print(row['FICHA'],row['DNI'],row['NOMBRE'],row['EMAIL'])
+    sql=f"INSERT INTO FICHAINSTRUCTOR(FICHA,DNI,NOMINST,EMAIL) VALUES('{row['FICHA']}','{row['DNI']}','{row['NOMBRE']}','{row['EMAIL']}')".format(row['FICHA'],row['DNI'],row['NOMBRE'],row['EMAIL'])
+    # print(sql)
+    Insertar(sql)
+
+print("PROCESO TERMINADO")
--- 4-DESARROLLO/app.py
+++ 4-DESARROLLO/app.py
@@ -25,6 +25,10 @@ def menu():
 @app.route('/login') 
 def login():   
     return render_template('login.html')
+
+@app.route('/acerca') 
+def acerca():   
+    return render_template('acerca.html')
 @app.route('/valida' ,methods=['POST','GET']) 
 def valida():   
     N=1
@@ -39,6 +43,8 @@ def valida():
     session['ficha'] = aprendiz[0]
     session['dnia'] = aprendiz[1]
     session['nombreap'] = aprendiz[2]
+    session['titulacion'] = aprendiz[6]
+    
     F=session['ficha']
     A=session['dnia']
     N=1
@@ -46,6 +52,31 @@ def valida():
     datos=Ejecutar(sql)
 
     return render_template('carga.html',N=N,datos=datos)
+
+@app.route('/otro' ,methods=['POST','GET']) 
+def otro():
+    hay=EjecutarUno(f"SELECT count(*) FROM FICHAPRENDIZ WHERE DNIA='{usua}' AND EMAIL='{pw}'".format(usua,pw))
+    hay=hay[0]
+    if hay=="0":
+        session[usua]=usua
+
+        return render_template("alertas.html",msgito="USUARIO O CLAVE INCORRECTO",regreso="/login")
+    sql=f"SELECT * FROM FICHAPRENDIZ WHERE EMAIL='{usua}' AND PWDAP='{pw}'".format(usua,pw)
+    aprendiz=EjecutarUno(sql)
+    session['ficha'] = aprendiz[0]
+    session['dnia'] = aprendiz[1]
+    session['nombreap'] = aprendiz[2]
+    session['titulacion'] = aprendiz[6]
+    F=session['ficha']
+    A=session['dnia']
+    N=1
+    sql=f"SELECT * FROM FICHAINSTRUCTOR WHERE DNI NOT IN(SELECT IDINSTRUCTOR FROM THEVAL WHERE IDFICHA='{F}' AND IDAPRENDIZ='{A}')".format(F,A)
+    datos=Ejecutar(sql)
+    session['instructor'] = datos[1]
+    session['ninstructor'] = datos[0]
+
+    return render_template('carga.html',N=N,datos=datos)
+
 @app.route('/evalua/<N>/<I>' ,methods=['POST','GET']) 
 def evalua(N,I):   
     if N=="1":
@@ -70,14 +101,18 @@ def evalua(N,I):
     if N=="3":
         F=session['ficha']
         A=session['dnia']
+        T=session['titulacion']
+        I=session['titulacion']
+        
         # I=session['instructor']
         print(F,I,A)
         conta = int(request.form.get('conta'))
         for i in range(1, conta + 1):  # Asegúrate de incluir el último valor
             Resp=request.form.get('R' + str(i))
             Preg=request.form.get('P' + str(i))
-            sql=f"insert into THEVAL(idINSTRUCTOR,idFICHA,idAPRENDIZ,PREGUNTA,RESPUESTA) VALUES({I},{F},{A},'{Preg}','{Resp}')".format(I,F,A,Preg,Resp,)
-            Insertar(sql)
+            sql=f"insert into THEVAL(idINSTRUCTOR,idFICHA,idAPRENDIZ,PREGUNTA,RESPUESTA,TITULACION) VALUES({I},{F},{A},'{Preg}','{Resp}','{T}')".format(I,F,A,Preg,Resp,T)
+            # Insertar(sql)
+            print(sql)
         
         msgito="Respuestas registrada"
         regreso="/login"

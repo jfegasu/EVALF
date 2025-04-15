b407316c38eea43780aa283c04475f4132f82e90
jfegasu <jfegasu@gmail.com>
2025-04-14 19:25:22 -0500

    feat(DEV) icono silueta

--- 4-DESARROLLO/Descarga.py
+++ 4-DESARROLLO/Descarga.py
@@ -3,7 +3,7 @@ from utils.Utilitarios import *
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATABASE = os.path.join(BASE_DIR, 'database', 'EVALF.db')
 print('DESCARGANDO RESPUESTAS')
-sql="SELECT * FROM THEVAL"
+sql="SELECT idFICHA,TITULACION,PREGUNTA,RESPUESTA FROM THEVAL"
 datos=pd.DataFrame(ConsultarD(DATABASE,sql))
 datos.to_csv("RESPUESTAS.csv",index=False)
 print("PROCESO TERMINADO")

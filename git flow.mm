ea2dc946641b7e7ffe71c3d9ba71ffdf8c320436
jfegasu <jfegasu@gmail.com>
2025-04-26 20:48:58 -0500

    diagrama de robustez

---
+++ git flow.mm
@@ -0,0 +1,8 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1745630550697" ID="ID_1903652777" MODIFIED="1745630574262" TEXT="git flow">
+<node CREATED="1745630586539" ID="ID_1691933288" MODIFIED="1745630595470" POSITION="right" TEXT="init"/>
+<node CREATED="1745630597909" ID="ID_406732864" MODIFIED="1745630638114" POSITION="right" TEXT="feature start &lt;nombre&gt;"/>
+<node CREATED="1745630639084" ID="ID_48585360" MODIFIED="1745630662962" POSITION="right" TEXT="git feature finish &lt;nombre&gt;"/>
+</node>
+</map>

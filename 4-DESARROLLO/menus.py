df135c0690153e8e9494752df57e6e2e4c94632f
jfegasu <jfegasu@gmail.com>
2025-05-18 17:56:44 -0500

    actualizar

--- 4-DESARROLLO/menus.py
+++ 4-DESARROLLO/menus.py
@@ -1,15 +1,3 @@
-# config.py
-class Config:
-    DEBUG = False
-    TESTING = False
-    SECRET_KEY = 'tu_clave_secreta_segura'
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
-    apidb="http://127.0.0.1:5555"
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    apidb="http://127.0.0.1:5555"
-
 menu = [
     {
         "titulo": "CONFIGURACION",
@@ -41,12 +29,3 @@ menu = [
         ]
     },
 ]
-   
-
-class ProductionConfig(Config):
-    SECRET_KEY = 'clave_muy_segura_en_producción'
-    # SQLALCHEMY_DATABASE_URI = 'mysql://usuario:password@servidor/db'
-
-class TestingConfig(Config):
-    TESTING = True
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
--- 4-DESARROLLO/utils/Utilitarios.py
+++ 4-DESARROLLO/utils/Utilitarios.py
@@ -287,3 +287,35 @@ def obtener_trimestreT(fecha):
     anual=fecha.year
     trimestre = (mes - 1) // 3 + 1
     return "T"+str(anual)+"-"+str(trimestre)
+
+menu = [
+    {
+        "titulo": "CONFIGURACION",
+        "items": [
+            {"texto": "DATOS INICIALES", "url": "/construir","svg":"","fa":"fa fa-address-book"},
+            {"texto": "APERTURA ENCUESTA", "url": "/construir","svg":"9211","fa":""}
+        ]
+    },
+    {
+        "titulo": "CARGUE DE DATOS",
+        "items": [
+            {"texto": "CARGA MASIVA", "url": "/CargaInicial","svg":"9981","fa":""},
+            {"texto": "APRENDICES", "url": "/construir","svg":"","fa":"fa fa-users"},
+            {"texto": "INSTRUCTORES", "url": "/construir","svg":"","fa":"fa fa-graduation-cap"},
+            {"texto": "PREGUNTAS", "url": "/construir","svg":"","fa":"fa fa-question"},
+        ]
+    },
+    {
+    "titulo": "RESULTADOS",
+        "items": [
+            {"texto": "EXPORTAR RESULTADOS(CSV)", "url": "/resp","svg":"","fa":"fa fa-table"},
+        ]
+    },
+        {
+    "titulo": "AUDITORIA",
+        "items": [
+            {"texto": "DESCARGA LOG TRANSACCIONES", "url": "/descargarlog","svg":"","fa":"fa fa-cloud-download"},
+            {"texto": "VER LOG DE TRANSACCIONES", "url": "/verlog","svg":"","fa":"fa fa-television"},
+        ]
+    },
+]

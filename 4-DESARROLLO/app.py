767096fd4e60e0b3eb576c9e79bb21caddfd3e47
jfegasu <jfegasu@gmail.com>
2025-06-08 12:02:13 -0500

    .

--- 4-DESARROLLO/app.py
+++ 4-DESARROLLO/app.py
@@ -400,46 +400,7 @@ def success():
         msgito="FOTO EDITADA"
         regreso="/foto"
         return render_template("alertas.html", msgito=msgito,regreso=regreso)   
-@app.route('/menu1', methods = ['GET'])   
-def menu1():
-    menu = [
-    {
-        "titulo": "CONFIGURACION",
-        "items": [
-            {"texto": "DATOS INICIALES", "url": "/construir","svg":"","fa":"fa fa-address-book"},
-            {"texto": "APERTURA ENCUESTA", "url": "/construir","svg":"9211","fa":""}
-        ]
-    },
-    {
-        "titulo": "CARGUE DE DATOS",
-        "items": [
-            {"texto": "CARGA MASIVA", "url": "/CargaInicial","svg":"9981","fa":""},
-            {"texto": "APRENDICES", "url": "/construir","svg":"","fa":"fa fa-users"},
-            {"texto": "INSTRUCTORES", "url": "/construir","svg":"","fa":"fa fa-graduation-cap"},
-            {"texto": "PREGUNTAS", "url": "/construir","svg":"","fa":"fa fa-question"},
-        ]
-    },
-    {
-    "titulo": "RESULTADOS",
-        "items": [
-            {"texto": "EXPORTAR RESULTADOS(CSV)", "url": "/resp","svg":"","fa":"fa fa-table"},
-        ]
-    },
-        {
-    "titulo": "AUDITORIA",
-        "items": [
-            {"texto": "DESCARGA LOG TRANSACCIONES", "url": "/descargarlog","svg":"","fa":"fa fa-cloud-download"},
-            {"texto": "VER LOG DE TRANSACCIONES", "url": "/verlog","svg":"","fa":"fa fa-television"},
-        ]
-    },
-    {
-        "titulo":"TERMINAR",
-        "items":[{"texto":"SALIR DEL APLICATIVO","url":"/saliendo","svg":"","fa":"fa fa-window-close"}]
-
-    }
-]
-
-    return render_template("menu1.html",menu=menu)   
+    # return render_template("menu1.html",menu=menu)   
 
 @app.route('/CargaInicial', methods = ['GET'])   
 def CargaInicial():

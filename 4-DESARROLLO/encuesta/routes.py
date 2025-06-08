4c2ddadab313d462d1f29da8dba780ecdbb0e3d8
jfegasu <jfegasu@gmail.com>
2025-06-07 20:27:52 -0500

    .

--- 4-DESARROLLO/encuesta/routes.py
+++ 4-DESARROLLO/encuesta/routes.py
@@ -6,5 +6,11 @@ eval = Blueprint('eval', __name__, template_folder='templates',static_folder='st
     static_url_path='/eval/static')
 
 @eval.route('/')
-def index():
-    return "Evaluacion"
+def index(): 
+    usua=session['usua']   
+    datos=requests.get(f'{apidb}/u/datos/{usua}').json()
+    ficha=requests.get(f'{apidb}/a/1/{usua}').text
+    session['datos']=datos
+    aa=f'{apidb}/i/2/{ficha}/{usua}'
+    datos=requests.get(aa).json()
+    return render_template('eval_carga.html',N=1,datos=datos,apr=session['datos'])

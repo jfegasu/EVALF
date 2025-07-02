cd4165c65715fb755c630803980681e8b18f4349
Jose Fegasu <jfegasu@gmail.com>
2025-07-02 07:56:39 -0500

    .

--- 4-DESARROLLO/database/pkgcnx/cnx.py
+++ 4-DESARROLLO/database/pkgcnx/cnx.py
@@ -1,17 +1,16 @@
 from flask import Flask,current_app
 from peewee import MySQLDatabase  
 
-MYSQL= MySQLDatabase(
+try:
+    MYSQL= MySQLDatabase(
 'evalf',
 user='root',
 password='',
 host='localhost',
 port=3306
 )
-db=MYSQL    
+    db=MYSQL 
+except Exception as e:
+    print("Fallo la conexion")
+   
 
-# current_app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-# current_app.config['MYSQL_DATABASE_USER'] = 'root'
-# current_app.config['MYSQL_DATABASE_PASSWORD'] = ''
-# current_app.config['MYSQL_DATABASE_DB'] = 'evalf'
-# current_app.config['apidb'] =  "http://127.0.0.1:5556"
--- 4-DESARROLLO/inicioapi.bat
+++ 4-DESARROLLO/inicioapi.bat
@@ -3,5 +3,5 @@ rem call venv\Scripts\Activate.bat
 
 @echo INICIANDO BASE DE DATOS
 cd 
-
 start python database\api1.py
+pause
